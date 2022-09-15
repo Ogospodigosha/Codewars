62e8676112a6b51cce32dec92ed623793d00469b
Ogospodigosha <odintsovis@hotmail.com>
2022-09-15 20:05:25 +0300

    32.js

---
+++ 32.js
@@ -0,0 +1,6 @@
+
+function arrayPlusArray(arr1, arr2) {
+  let  sumArr1 = arr1.reduce((prev,item)=> prev + item)
+  let  sumArr2 = arr2.reduce((prev,item)=> prev + item)
+    return sumArr1 + sumArr2
+}
