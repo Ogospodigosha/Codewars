35e31ac26c70ac873b050bef3829b13c5e056bca
Ogospodigosha <odintsovis@hotmail.com>
2022-11-24 23:31:08 +0300

    49.js

---
+++ 49.js
@@ -0,0 +1,19 @@
+const count =(string) =>{
+    if (string === "") {
+        return {}
+    }
+    let rezult = {}
+    for (let i=0; i<string.length; i++) {
+        if (rezult[string[i]] === undefined) {
+            rezult[string[i]] = 1
+        } else {
+            rezult[string[i]] = rezult[string[i]] +1
+        }
+
+
+    }
+    return rezult
+}
+console.log(count('Gosahaa'))
+
+
